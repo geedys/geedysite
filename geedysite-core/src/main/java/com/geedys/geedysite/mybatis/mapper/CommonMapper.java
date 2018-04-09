8030199a559b8aeee14cfce783471becfb2beab3
moonshy <soutnila@msn.com>
2018-04-09 11:16:18 +0800

    使用通用Mapper的@RegisterMapper注解到公共父Mapper上。配置文件中可不用配置mapper.mappers参数

--- geedysite-core/src/main/java/com/geedys/geedysite/mybatis/mapper/CommonMapper.java
+++ geedysite-core/src/main/java/com/geedys/geedysite/mybatis/mapper/CommonMapper.java
@@ -1,9 +1,11 @@
 package com.geedys.geedysite.mybatis.mapper;
 
+import tk.mybatis.mapper.annotation.RegisterMapper;
 import tk.mybatis.mapper.common.*;
 
 /**
  * @author resigshy Created on 2016-12-16 14:45
  */
+@RegisterMapper
 public interface CommonMapper<T, ID> extends Mapper<T>, IDListMapper<T, ID>, IdsMapper<T> {
 }
